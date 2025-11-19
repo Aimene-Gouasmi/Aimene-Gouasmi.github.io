20a7d772d1959703ee1a2a8231ce9fefd34d25a6
Aimene-Gouasmi <117349345+Aimene-Gouasmi@users.noreply.github.com>
2025-11-19 17:21:01 +0100

    Create talk&_1.d

---
+++ _talks/talk&_1.d
@@ -0,0 +1,17 @@
+---
+title: "Flux reconstruction for interface problems and a posteriori error analysis"
+collection: talks
+type: "Seminar"
+venue: "Applied Mathematics Seminar, University of Pau"
+date: 2024-06-15
+location: "Pau, France"
+---
+
+This seminar presented my PhD work on **flux reconstruction techniques** for elliptic interface problems, focusing on:
+
+- construction of equilibrated fluxes in heterogeneous media  
+- handling discontinuous coefficients and geometric singularities  
+- guaranteed and local a posteriori error estimators  
+- applications to adaptive mesh refinement and finite element computations
+
+Numerical examples were shown using Python/FEniCS to illustrate the robustness of the estimators.
